06960e16303a55ca2e355111d2ebdb44634fb52f
Oderlan Freire <56801164+oderlanfreire@users.noreply.github.com>
2022-03-24 17:01:12 -0300

    arquivos da aula 1 e 2 upados

---
+++ python/aula2/aula02.py
@@ -0,0 +1,11 @@
+a = int(input('Insira o primeiro valor: '));
+b = int(input('Insira o segundo valor: '));
+
+soma = a + b;
+subtracao = a - b;
+divisao = a/b;
+multiplicacao = a * b;
+resto = a%b;
+
+print('soma: {soma}. \nsubtração: {sub}. \ndivisão: {divi}. \nmultiplicação: {mult}. \nresto: {resto}' .format(soma = soma,
+sub = subtracao,divi= divisao,mult = multiplicacao,resto = resto));
