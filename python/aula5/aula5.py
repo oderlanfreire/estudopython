a4e29bba34901be922761d9e2f1330362fe3c911
oderlan <oderlanfreire@gmail.com>
2022-03-30 11:54:50 -0300

    Aula 5 concluida e conteudo feito durante a aula adicionado

---
+++ python/aula5/aula5.py
@@ -0,0 +1,44 @@
+lista = [19, 2, 38, 14, 5]
+lista_jogos = ['Zelda - Triforce', 'Digimon World 3',
+               'Castlevania - SOTN', 'Naruto Shippuden 2']
+
+lista_jogos[0] = 'Yu-Gi-Oh - Forbidden'
+print(lista_jogos)
+
+
+lista_tupla1 = (1, 25, 9, 44, 255)
+print(lista_tupla1)
+print(len(lista_tupla1))
+
+tupla_jogos = tuple(lista_jogos)
+print(tupla_jogos)
+
+lista_num = list(lista_tupla1)
+print(lista_num)
+# print(lista)
+
+# print(lista_jogos[1])
+
+# for x in lista_jogos:
+#    print(x)
+
+# print(sum(lista))
+
+# if 'Bomberman' in lista_jogos:
+#    print('Bomberman já esta na lista de jogos')
+# else:
+#    print("Bomberman não existe, o mesmo será incluido na lista")
+#    lista_jogos.append('Bomberman')
+#    print(lista_jogos)
+
+# lista_jogos.pop()
+# print(lista_jogos)
+
+# lista.sort()
+# lista_jogos.sort()
+
+# print(lista)
+# print(lista_jogos)
+
+# lista_jogos.reverse()
+# print(lista_jogos)
